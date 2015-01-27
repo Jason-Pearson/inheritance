2ac102cd7f6a04be7bb6942567a5ee9ec9e9c954
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-01-27 14:08:47 -0500

    Initial Commit

---
+++ Lesson4/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Hero
+    {
+        //Private Properties 
+        private bool bravery = true;
+        private int health;
+        private int smarts;
+        private string gender;
+
+        //Constructor class*******************************
+        public Hero(int health, int smarts, string gender)
+        {
+            this.health = health;
+            this.smarts = smarts;
+            this.gender = gender;
+        }
+
+        public void doesGoodThings()
+        {
+            Console.WriteLine("Hero does good things...yeah...");
+        }
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero myHero = new Hero(7, 8, "female");
+            myHero.doesGoodThings();
+
+            Console.WriteLine("press to exit");
+            Console.ReadKey();
+        }
+    }
+}
